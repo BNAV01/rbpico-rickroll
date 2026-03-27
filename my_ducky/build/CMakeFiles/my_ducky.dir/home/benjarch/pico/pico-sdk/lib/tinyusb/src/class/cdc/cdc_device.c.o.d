f3c11f1591be1d1d5aebb3711a051d35ed9e5cd9
BNAV01 <bnavm01@gmail.com>
2026-03-27 12:12:18 -0300

    lil changes

--- my_ducky/build/CMakeFiles/my_ducky.dir/home/benjarch/pico/pico-sdk/lib/tinyusb/src/class/cdc/cdc_device.c.o.d
+++
@@ -1,6 +0,0 @@
-CMakeFiles/my_ducky.dir/home/benjarch/pico/pico-sdk/lib/tinyusb/src/class/cdc/cdc_device.c.o: \
- /home/benjarch/pico/pico-sdk/lib/tinyusb/src/class/cdc/cdc_device.c \
- /home/benjarch/pico/pico-sdk/lib/tinyusb/src/tusb_option.h \
- /home/benjarch/pico/pico-sdk/lib/tinyusb/src/common/tusb_compiler.h \
- /home/benjarch/pico/my_ducky/tusb_config.h \
- /home/benjarch/pico/pico-sdk/lib/tinyusb/src/common/tusb_mcu.h
--- my_ducky/build/_deps/picotool-build/data_locs.cpp
+++
@@ -1,7 +0,0 @@
-
-#include <vector>
-#include <string>
-
-std::vector<std::string> data_locs = {
-    "./","/home/benjarch/pico/my_ducky/build/_deps/picotool/"
-};
